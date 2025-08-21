8fce6df8d217d67bf9df9b86374df7d4faf922d6
MohithNakka <86645563+mohith1976@users.noreply.github.com>
2025-08-21 20:21:54 +0530

    controller issue\Just written the server.js and tested the server running or not

--- controllers/schoolController.js
+++ controllers/schoolController.js
@@ -1,7 +1,7 @@
-const { db } = require('../config/db');
+const db = require('../config/db');
 const Joi = require('joi');
 
-exports.addSchool = (req, res) => {
+exports.addSchool = async (req, res) => {
     const schema = Joi.object({
         name: Joi.string().min(3).required(),
         address: Joi.string().min(5).required(),
@@ -15,13 +15,19 @@ exports.addSchool = (req, res) => {
     }
 
     const { name, address, latitude, longitude } = req.body;
-    const query = 'INSERT INTO schools (name, address, latitude, longitude) VALUES (?, ?, ?, ?)';
-    
-    db.query(query, [name, address, latitude, longitude], (err, result) => {
-        if (err) {
-            console.error('DB Insert Error:', err);
-            return res.status(500).json({ error: 'Database insert failed' });
-        }
-        res.status(201).json({ message: 'School added successfully', schoolId: result.insertId });
-    });
+
+    try {
+        const [result] = await db.query(
+            'INSERT INTO schools (name, address, latitude, longitude) VALUES (?, ?, ?, ?)',
+            [name, address, latitude, longitude]
+        );
+
+        res.status(201).json({
+            message: 'School added successfully',
+            schoolId: result.insertId,
+        });
+    } catch (err) {
+        console.error('DB Insert Error:', err);
+        res.status(500).json({ error: 'Database insert failed' });
+    }
 };
